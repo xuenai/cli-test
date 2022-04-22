0910dcbf77ee6d5d7415f3ecb6044061380719d9
xuenai <18381671514@163.com>
2022-04-22 08:56:17 +0800

    yargs

--- cli-test-xxx/bin/index.js
+++ cli-test-xxx/bin/index.js
@@ -1,11 +1,53 @@
 #!/usr/bin/env Node
-const lib = require('cli-test-xxx-lib')
-const argv = require('process').argv;
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 
-const command = argv[2]
+const argv = process.argv.slice(2)
+const pkg = require("../package.json");
+const context = {
+  xthjVersion: pkg.version,
+};
 
-if (command.startsWith('--') || command === '-v') {
-  console.log('v1.0.0')
-} else {
-  console.log(command)
-}
+const cli = yargs()
+
+cli.usage("Usage: hj <command> [options]")
+.strict()
+.demandCommand(1, "A command is required. Pass --help to see all available commands and options.")
+.alias("h", "help")
+.alias("v", "version")
+.wrap(cli.terminalWidth())
+.epilogue('xthj forever !!!')
+.options({
+  debug: {
+    type: 'boolean',
+    describe: 'bootstrap debug mode',
+    alias: 'd'
+  }
+})
+.group(['d'], 'Dev options:')
+.command('init [name]', 'Do init a project', (yargs) => {
+  yargs.option('name', {
+    type: 'string',
+    describe: 'name of a project',
+    alias: 'n'
+  })
+}, (argv) => {
+  console.log(argv)
+})
+.command({
+  command: 'list [name]',
+  aliases: ['ls'],
+  describe: 'list a list',
+  builder: function(yargs) {
+    yargs.option('name', {
+      type: 'string',
+      describe: 'name of a list',
+      alias: 'n'
+    })
+  },
+  handler(argv) {
+    console.log(argv)
+  }
+})
+.recommendCommands()
+.parse(argv, context);
